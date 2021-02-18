b4470e7f1f1ce1bfb95c563d98ade870d94470e8
AmandaHolliday <amandamholliday@gmail.com>
2021-02-18 01:43:41 -0500

    edited wireframe slides and took off captions

--- portfolio/src/Components/Contact.js
+++ portfolio/src/Components/Contact.js
@@ -10,8 +10,8 @@ function contact() {
             <img src="https://see.fontimg.com/api/renderfont4/pALD/eyJyIjoiZnMiLCJoIjo2NSwidyI6MTAwMCwiZnMiOjY1LCJmZ2MiOiIjM0EzODM4IiwiYmdjIjoiI0ZGRkZGRiIsInQiOjF9/Q29udGFjdCBNZQ/wonderbar.png"/>
             <h2>Email me:</h2>
             <h3>amandamholliday@gmail.com</h3>
-            <i class="fab fa-linkedin">linkedin</i> <br />
-            <i class="fab fa-github">github</i>
+            <i class="fab fa-linkedin"><a  target="_blank" href="https://www.linkedin.com/in/amanda-holliday-b3b07760/">linkedin</a></i> <br />
+            <i class="fab fa-github"><a target="_blank" href="https://github.com/amandamholliday">github</a></i>
         </div>
     )
 }
--- portfolio/src/Components/Wireframes.js
+++ portfolio/src/Components/Wireframes.js
@@ -17,11 +17,6 @@ function wireframes() {
                         src="https://res.cloudinary.com/dzgeqcksj/image/upload/v1613519679/Screen_Shot_2021-02-02_at_3.48.53_PM_hbb4cl.png"
                         alt="Third slide"
                         />
-
-                        <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                        </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
@@ -29,11 +24,6 @@ function wireframes() {
                         src="https://res.cloudinary.com/dzgeqcksj/image/upload/v1613519336/Screen_Shot_2021-01-20_at_11.00.18_AM_uy2poc.png"
                         alt="Third slide"
                         />
-
-                        <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
@@ -41,11 +31,6 @@ function wireframes() {
                         src="https://res.cloudinary.com/dzgeqcksj/image/upload/v1613519529/Screen_Shot_2021-01-20_at_11.00.36_AM_yrcxa2.png"
                         alt="Third slide"
                         />
-
-                        <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                        </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
@@ -53,11 +38,6 @@ function wireframes() {
                         src="https://res.cloudinary.com/dzgeqcksj/image/upload/v1613519485/Screen_Shot_2021-01-20_at_11.00.02_AM_oy5qed.png"
                         alt="Third slide"
                         />
-
-                        <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                        </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
@@ -65,11 +45,6 @@ function wireframes() {
                         src="https://res.cloudinary.com/dzgeqcksj/image/upload/v1613519422/Screen_Shot_2021-01-20_at_1.15.06_PM_ut9vex.png"
                         alt="Third slide"
                         />
-
-                        <Carousel.Caption>
-                        <h3>The Record Collection</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                        </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
@@ -77,10 +52,6 @@ function wireframes() {
                         src="https://res.cloudinary.com/dzgeqcksj/image/upload/v1613580847/sepi_n9qldw.png"
                         alt="First slide"
                         />
-                        <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
             </div>
